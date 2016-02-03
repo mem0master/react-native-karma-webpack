815cd6cc1d646724dd4856cd8cc36dfb55ebf34d
Mohamed Mellouki <mohamed.mellouki@osedea.com>
2016-02-03 04:10:42 -0500

    refactored to have only webpack & karma dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,48 @@
 'use strict';
 
-var path = require('path');
-var args = require('minimist')(process.argv.slice(2));
+let path = require('path');
+let BowerWebpackPlugin = require('bower-webpack-plugin');
 
-// List of allowed environments
-var allowedEnvs = ['test'];
-
-// Set the correct environment
-var env;
-
-process.env.REACT_WEBPACK_ENV = 'test';
-
-module.exports = require(path.join(__dirname, 'cfg/test'));
+module.exports = {
+  devtool: 'eval',
+  module: {
+    preLoaders: [
+        {
+          test: /\.(js|jsx)$/,
+          loader: 'babel-loader',
+          exclude: /node_modules/,
+          query: {
+              "presets": ["react", "es2015"]
+          }
+        }
+    ],
+    loaders: [
+      {
+        test: /\.(png|jpg|gif|woff|woff2|css|sass|scss|less|styl)$/,
+        loader: 'null-loader'
+      },
+      {
+        test: /\.(js|jsx)$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+            "presets": ["react", "es2015"]
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: [ '', '.js', '.jsx' ],
+    root: [
+        path.resolve('app/')
+    ],
+    alias: {
+      helpers: path.join(__dirname, '/../test/helpers'),
+    }
+  },
+  plugins: [
+    new BowerWebpackPlugin({
+      searchResolveModulesDirectories: false
+    })
+  ]
+};
